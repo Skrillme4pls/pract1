2404c5bfcd879a2f6884e8cb2795de1e77eade7a
Skrillme4pls <kib22-o.zhenzherya@nubip.edu.ua>
2023-07-04 14:11:14 +0300

    first commit

--- exercise_1/main.c
+++ exercise_1/main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
+// Функція для знаходження НСД (Найбільший спільний дільник)
+int findGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// Функція для знаходження НСК (Найменше спільне кратне)
+int findLCM(int a, int b) {
+    return (a * b) / findGCD(a, b);
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int p;
+    int numbers[20];
+
+    // Зчитуємо кількість чисел
+    scanf("%d", &p);
+
+    // Зчитуємо натуральні числа
+    for (int i = 0; i < p; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    // Знаходимо НСК для заданих чисел
+    int lcm = numbers[0];
+    for (int i = 1; i < p; i++) {
+        lcm = findLCM(lcm, numbers[i]);
+    }
+
+    // Виводимо результат
+    printf("%d\n", lcm);
+
     return 0;
 }
